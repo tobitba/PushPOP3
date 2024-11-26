75e2988341cfcc049fc47108487d2e72d3f41773
berni-245 <bernizapico@gmail.com>
2024-11-26 03:14:10 -0300

    merged

--- include/commands.h
+++ include/commands.h
@@ -3,9 +3,6 @@
 
 #include "pop3.h"
 
-// We could add more argument if needed, and maybe mix arg1 and the bool in an struct.
-typedef state (*handler)(pop3* data, char* arg, bool isArgPresent);
-
 typedef struct CommandCDT* Command;
 
 Command getCommand(buffer* b, const state current);
